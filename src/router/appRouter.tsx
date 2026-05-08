86937d3162dcc5db7bcc2132bb53dddf202402d3
Karima-Haddad <karima.haddad@etudiant-fst.utm.tn>
2026-05-08 21:12:18 +0100

    CLEAN

--- src/router/appRouter.tsx
+++ src/router/appRouter.tsx
@@ -42,7 +42,7 @@ export const router = createBrowserRouter([
     children: [
       {
         index: true,
-        element: <Navigate to="/mutex/lamport" replace />,
+        element: <Navigate to="/clocks/lamport" replace />,
       },
 
       {
